97fb0945790be076b94d3350a533e767fa97b152
Vedamir <grandros1@gmail.com>
2023-01-20 20:28:49 +0800

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число (k): ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(k>m && k>n)
+{
+    Console.WriteLine(k);
+    }
+else if(m>=n)
+ Console.WriteLine(m);
+ else
+  Console.WriteLine(n);
+ 
