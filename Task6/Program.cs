0062e212345c8f51d73f177a902350c02295de83
Vedamir <grandros1@gmail.com>
2023-01-20 20:37:05 +0800

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿   Console.Write("Введите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ if(n %2 == 0)
+    Console.WriteLine("Число четное");
+ else
+    Console.WriteLine("Число нечетное");
