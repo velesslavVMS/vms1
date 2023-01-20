09de345994cc2f46e2e147a66139a6bdc2b9d21d
Vedamir <grandros1@gmail.com>
2023-01-20 20:14:40 +0800

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите первое число (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n>m)
+    Console.WriteLine("n>m");
+else if (m>n)
+    Console.WriteLine("m>n");
+else
+    Console.WriteLine("числа равны");
+
+
+
+
